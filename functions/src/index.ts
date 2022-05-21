1363745919e121064b6638b50edaa4345055517b
Wilson <43991506+WilsonLe@users.noreply.github.com>
2022-05-20 22:03:27 -0400

    implement delete old anon users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,6 +20,7 @@ export { default as createFCMToken } from './user/createFCMToken';
 export { default as createUserIfNotExist } from './user/createUserIfNotExist';
 export { default as deleteAnonymousUser } from './user/deleteAnonymousUser';
 export { default as deleteFCMToken } from './user/deleteFCMToken';
+export { default as deleteOldAnonUsers } from './user/deleteOldAnonUsers';
 export { default as getUserProfile } from './user/getUserProfile';
 export { default as messageCoreTeam } from './user/messageCoreTeam';
 export { default as sendSignInEmail } from './user/sendSignInEmail';
