62b021fd7eadc30e0aa34d936bb6a9296c458695
Wilson <43991506+WilsonLe@users.noreply.github.com>
2022-08-23 10:57:52 +0700

    save

--- functions/src/models/errors/email.ts
+++
@@ -1,8 +0,0 @@
-import { Validator } from '../../utils/validator';
-
-export class Email extends String {
-	constructor(field: string, input: unknown) {
-		Validator.string(field, input);
-		super(input);
-	}
-}
